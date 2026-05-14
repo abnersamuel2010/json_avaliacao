6dbb4b4bf4f56aaff26c637ee68cd4aae5d6d755
Abner <tomaz.abner@escola.pr.gov.br>
2026-05-14 12:01:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -18,13 +18,36 @@ async function pagina() {
                 <td> ${item.descricao} </td>
                 <td> ${item.apelido} </td>
                 <td> ${item.sobrenome} </td>
-                <td><button>editar</button></td>
+                <td><button onclick="removerLinha(this)">remover</button></td>
+                <td><button onclick="adicionarLinha(this)">adicionar</button></td>
             `;
 
             destino.appendChild(card);
+
+            
         });
     } catch (error) {
         console.error('Erro na requisição:', error);
         destino.innerHTML = '<p>Desculpe, não foi possivel carregar as informações no momento.</p>';
     }
  }
+function removerLinha(botao){
+    const linha = botao.closest('tr'); 
+    linha.remove();
+}
+function adicionarLinha(sla){
+    const linha = sla.closest('tr');
+
+    let celulas = linha.cells
+
+    let input1 = document.getElementById("input1");
+    let input2 = document.getElementById("input2");
+    let input3 = document.getElementById("input3");
+    let input4 = document.getElementById("input4");
+
+    input1.value = celulas[0].innerHTML;
+    input2.value = celulas[1].innerHTML;
+    input3.value = celulas[2].innerHTML;
+    input4.value = celulas[3].innerHTML;
+
+}
